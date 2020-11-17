6a10b590afb66688bace8935c1976f92967d75e8
dedeibel <dedeibel@arcor.de>
2020-11-17 19:48:44 +0100

    Improve help dialog, invert tip index printing

--- main.py
+++ main.py
@@ -34,22 +34,22 @@ if __name__ =='__main__':
     parser = argparse.ArgumentParser(description='Generate good(TM) lotto numbers.')
     parser.add_argument('numbers', metavar='NUMBERS', type=str, nargs=1,
                        help='The amount of numbers you want')
-    parser.add_argument('-v', '--verbose', type=int, default='1',
-                       help='Chattyness')
-    parser.add_argument('-t', '--test', dest='test_distribution', action='store_true',
-                       help='Tests the distribution of n generated and filtered results, use e.g. 10000')
+    parser.add_argument('-d', '--dist', dest='show_distribution', action='store_true',
+                       help='Additionally shows the number distribution of the generated tips')
     parser.add_argument('-f', '--find', dest='find_numbers', action='store_true',
                        help='Search for the given numbers in the journal four or more matches, comma separated, no spaces as NUMBERS parameter')
-    parser.add_argument('-b', '--no-blacklist', dest='no_blacklist', action='store_true',
-            help='Ignores the blacklist, kind of useless except for testing')
     parser.add_argument('-j', '--no-journal', dest='no_journal', action='store_true',
             help='Does not write to journal ~/.quicktipp.log in this run (also does not in -t or -d)')
+    parser.add_argument('-p', '--no-prefix', dest='no_prefix', action='store_true',
+            help='Prevent showing an index before each tip')
+    parser.add_argument('-t', '--test', dest='test_distribution', action='store_true',
+                       help='Tests the distribution of n generated and filtered results, use e.g. 10000')
     parser.add_argument('-2', '--two-dee', dest='two_dee', action='store_true', # ;-)
             help='Print distribution histogramm as 2D map of the number coordinates')
-    parser.add_argument('-n', '--numbers', dest='print_numbers', action='store_true',
-            help='Prints an index before the numbers')
-    parser.add_argument('-d', '--dist', dest='show_distribution', action='store_true',
-                       help='Shows the distribution of the normally generated and displayed results')
+    parser.add_argument('-b', '--no-blacklist', dest='no_blacklist', action='store_true',
+            help='Ignores the blacklist, kind of useless except for testing')
+    parser.add_argument('-v', '--verbose', type=int, default='1',
+                       help='Chattyness 0 to 3')
     
     args = parser.parse_args()
     numbers = args.numbers[0]
@@ -67,7 +67,7 @@ if __name__ =='__main__':
     q = Quicktipp()
     q.set_ignore_blacklist(args.no_blacklist)
     q.set_verbose(args.verbose)
-    q.set_print_index_numbers(args.print_numbers)
+    q.set_print_index_numbers(not args.no_prefix)
     q.prepare(numbers)
     print(str(q))
 
